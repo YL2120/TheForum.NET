c9d070a40f3fda743a8484fabbd1b158664c7eca
Yassine Layachi <yassinelayachi@outlook.com>
2021-12-07 16:51:15 +0100

    starting topics page

---
+++ TheForum.NET/Controllers/TopicsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheForum.Data;
+
+namespace TheForum.NET.Controllers
+{
+    public class TopicsController : Controller
+    {
+        private readonly TheForumContext context;
+
+        public TopicsController(TheForumContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- TheForum.NET/Program.cs
+++ TheForum.NET/Program.cs
@@ -56,4 +56,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+
+
 app.Run();
